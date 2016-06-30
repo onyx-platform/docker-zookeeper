bd52c3965e10e2a0fd15d8f3b98ad2a837895340
Gardner Vickers <gardner@vickers.me>
2016-06-30 13:02:45 -0400

    initial

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM java:openjdk-8-jre-alpine
+# Sourced from Franck Cuny <franck.cuny@gmail.com> 
+# https://github.com/franckcuny/docker-distributedlog
+
+MAINTAINER Gardner Vickers <gardner.vickers@onyxplatform.org>
+
+RUN apk add --no-cache curl bash \
+    && mkdir -p /opt \
+    && cd /opt \
+    && curl -fL http://archive.apache.org/dist/zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz | tar xzf - -C /opt && \
+    mv /opt/zookeeper-3.4.8 /opt/zookeeper
+
+ENV LOG_DIR "/var/log/zookeeper"
+ENV ZK_DATA_DIR "/var/log/zookeeper"
+
+ADD zk-docker.sh /usr/local/bin/
+
+RUN ["mkdir", "-p", "/var/log/zookeeper", "/var/lib/zookeeper"]
+RUN ["chmod", "+x", "/usr/local/bin/zk-docker.sh"]
+
+VOLUME ["/var/log/zookeeper", "/var/lib/zookeeper"]
+
+EXPOSE 2181 2888 3888
+
+ENTRYPOINT ["/usr/local/bin/zk-docker.sh"]
+
+CMD ["/opt/zookeeper/bin/zkServer.sh", "start-foreground"]
---
+++ zk-docker.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+ZK_CFG_FILE="/opt/zookeeper/conf/zoo.cfg"
+
+: ${zk_id:=1}
+: ${zk_tickTime:=2000}
+: ${zk_initLimit:=5}
+: ${zk_syncLimit:=2}
+: ${zk_clientPort:=2181}
+: ${zk_maxClientCnxns:=0}
+
+export zk_dataDir='/var/lib/zookeeper'
+
+export zk_id
+export zk_tickTime
+export zk_initLimit
+export zk_syncLimit
+export zk_clientPort
+export zk_maxClientCnxns
+
+# Download the config file, if given a URL
+if [ ! -z "$ZK_CFG_URL" ]; then
+  echo "[zk] Downloading zk config file from ${ZK_CFG_URL}"
+  curl --location --silent --insecure --output ${ZK_CFG_FILE} ${ZK_CFG_URL}
+  if [ $? -ne 0 ]; then
+    echo "[zk] Failed to download ${ZK_CFG_URL} exiting."
+    exit 1
+  fi
+else
+  cat > "${ZK_CFG_FILE}" <<EOF
+tickTime=${zk_tickTime}
+initLimit=${zk_initLimit}
+syncLimit=${zk_syncLimit}
+dataDir=${zk_dataDir}
+clientPort=${zk_clientPort}
+EOF
+
+fi
+
+# Set Zookeeper ID
+echo $zk_id > $zk_dataDir/myid
+
+# set the servers in the conf
+if [ -n "$SERVERS" ]; then
+    printf '%s' "$SERVERS" | awk 'BEGIN { RS = "," }; { printf "server.%i=%s:2888:3888\n", NR, $0 }' >> /opt/zookeeper/conf/zoo.cfg
+fi
+
+env
+
+cat "${ZK_CFG_FILE}"
+
+exec $@
