80c21535483b8b7eb244f854cb6a416037a8cd23
Gardner Vickers <gardner@vickers.me>
2016-07-14 11:41:29 -0400

    add file, dont dynamically build it

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ RUN mkdir -p /workdir && \
 ADD on-start.sh /workdir/on-start.sh
 ADD install.sh /workdir/install.sh
 ADD log4j.properties /tmp/log4j.properties
+ADD zoo.cfg /tmp/zoo.cfg
 ADD https://storage.googleapis.com/kubernetes-release/pets/peer-finder /workdir/peer-finder
 
 RUN chmod -c 755 /workdir/on-start.sh /workdir/peer-finder
--- install.sh
+++ install.sh
@@ -1,17 +1,4 @@
 #! /bin/bash
 
-## Shared configuration directory for all bootstrapping
-
-touch /opt/zookeeper/conf/zoo.cfg
-echo "tickTime=2000" >> /opt/zookeeper/conf/zoo.cfg
-echo "initLimit=10" >> /opt/zookeeper/conf/zoo.cfg
-echo "syncLimit=5" >> /opt/zookeeper/conf/zoo.cfg
-echo "dataDir=/tmp/zookeeper" >> /opt/zookeeper/conf/zoo.cfg
-echo "dataLogDir=/zookeeper-transactions" >> /opt/zookeeper/conf/zoo.cfg
-echo "autopurge.snapRetainCount=5" >> /opt/zookeeper/conf/zoo.cfg
-echo "autopurge.purgeInterval=24" >> /opt/zookeeper/conf/zoo.cfg
-
-echo "standaloneEnabled=false" >> /opt/zookeeper/conf/zoo.cfg
-echo "dynamicConfigFile=/opt/zookeeper/conf/zoo.cfg.dynamic" >> /opt/zookeeper/conf/zoo.cfg
-
+mv /tmp/zoo.cfg /opt/zookeeper/conf/zoo.cfg
 mv /tmp/log4j.properties /opt/zookeeper/conf/log4j.properties
